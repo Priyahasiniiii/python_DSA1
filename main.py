b011f429eea1257d833c48f3e776dbf10707a14c
ShabhanaShaik <shabana01shaik@gmail.com>
2026-07-20 12:04:18 +0530

    Day 1

---
+++ main.py
@@ -0,0 +1,66 @@
+# print("Hasini\n Hasini")
+#print(r"\\tcurent\\new\\folder")
+# / is escape character
+#write a program to find odd and even with function:
+#def check (num):
+    #if num%2==0:
+        #print("Even")
+    #else:
+        #print("odd")
+#check(10)
+#l=[1,2,3,4,5]
+#l.pop(2)
+#print(l)
+#dict={
+    #"name":"Hasini",
+    #"gender":"female",
+    #"age":20,
+    #"courses":["python","java","datascience"]
+#}
+#dict["name"]="Hasini priya"
+#print(dict)
+#def count(*args):
+    #print(type(args))
+#count(1,2,3,4,5)
+#def dicts(**kwargs):
+    #print(type(kwargs))
+#dicts(name="hasi",age=20,gender="male")
+#def default(gender,age,name="hasi"):
+    #print(name,age,gender)
+
+#OOPS 
+#l=[1,2,3]
+#s="string"
+#len(l)
+#len(s) 
+#take a string as input and
+#print it back by removing the first and last character of input string
+#x="hasini"
+#x=x[1:-1]
+#x=x[::-1]
+#print(x)
+#while True:
+    #print("hi")
+#Take a number as input and find the sum of numbers from 1 to that number
+x=int(input("num:"))
+sum=0
+for i in range(1,x+1):
+    sum+=i
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
