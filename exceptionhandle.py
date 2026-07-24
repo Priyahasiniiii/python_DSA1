0b98517f73b61148e60f66516769cebf49dd13e9
Priyahasiniiii <priyahasini814@gmail.com>
2026-07-24 21:17:25 +0530

    Day 2

---
+++ exceptionhandle.py
@@ -0,0 +1,18 @@
+# x=int(input("Enter a number:"))
+# y=int(input("Enter another number:"))
+# try:
+#     print(x/y)
+# except ZeroDivisionError as e:
+#     print(e)
+# except ValueError as e:
+#     print(e)
+# finally:
+#     print("done")
+
+#Raising error
+# a=int(input("Enter a number:"))
+# if a<0:
+#     raise ValueError("Negative value is not allowed")
+# else:
+#     print(a)
+
